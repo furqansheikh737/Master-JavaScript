cedad4cf40ffba09e2dd15f65d4e2e897a6307ba
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-20 23:40:02 +0500

    Conditional statement & ternary operator done

--- 02_operators & conditional statements/Script.js
+++ 02_operators & conditional statements/Script.js
@@ -1,4 +1,4 @@
-// Arthimatic Operators
+//                                                    Arthimatic Operators
 
 // let a = 5;
 // let b = 3;
@@ -12,7 +12,7 @@
 // console.log("a ** b =", a ** b);  //exponentiation
 
 
-// Unary Operator
+//                                                         Unary Operator
 
 // let a = 5;
 // let b = 2;
@@ -32,7 +32,7 @@
 // console.log(a);
 
 
-// Assignment Operators
+//                                                         Assignment Operators
 
 // let a = 5;
 // let b = 2;
@@ -41,7 +41,7 @@
 // a **= 4 // a = a ** 4 = 625
 
 
-// Comparison Oprators
+//                                                           Comparison Oprators
 
 // let a = 5;
 // let b = 2;
@@ -65,7 +65,7 @@
 // console.log("5 >= 5", c >= d);
 
 
-// Logical Oprators
+//                                                              Logical Oprators
 // let a = 6;
 // let b = 5;
 
@@ -74,5 +74,58 @@
 // console.log("cond1 || cond2", a > b || a === 4)  // && : ye oprator dekhta hai k koi bhi aik condition true hui tw result true hoga baqi false.
 
 // console.log("cond1 ! cond2", !(a === 6))  // && : ye oprator true value false or false value ko true kr deta hai.
+
+
+//                                                            Conditional Statement
+
+// let mode = "orange";
+// let color; 
+
+// if(mode === "dark"){
+//     color = "black";
+// }else if(mode === "red"){
+//     color = "red";
+// }else if(mode === "yellow"){
+//     color = "yellow";
+// }else{
+//     color = "white";
+// }
+// console.log(color);
+
+
+//                                                               ternary Operators
+
+// age = 17
+
+// age > 18? console.log("Allow") : console.log("Not Allow")
+
+
+//                                                                Let's Practice
+// Q1. Get user to input a number using prompt("Enter a number;") Check if the number is a multiple of 5 or not.
+
+// let userInput = prompt("Enter a number:")
+
+// if(userInput % 5 === 0){
+//    alert(`${userInput} is a multiple of 5`)
+// }else{
+//     alert(`${userInput} is not a multiple of 5`)
+// }
+
+// Q2. Write a code can give grades to students according to their scores.
+
+
+let input = prompt("Enter a percentage");
+
+if(input <= 100 && input >= 80){
+   alert("Your grade A")
+}else if(input <= 79 && input >= 70){
+    alert("Your grade B")
+}else if(input <= 69 && input >= 60){
+    alert("Your grade C")
+}else if(input <= 59 && input >= 50){
+    alert("Your grade D")
+}else if(input <= 49 && input >= 0){
+    alert("Your grade F")
+}
 
 
