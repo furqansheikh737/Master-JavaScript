280af8a97595533afa1e08c5a9032dec61c3efa7
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-20 20:59:35 +0500

    all operators done

---
+++ 02_operators & conditional statements/Script.js
@@ -0,0 +1,78 @@
+// Arthimatic Operators
+
+// let a = 5;
+// let b = 3;
+
+// console.log("a = ", a, "& b = ", b)
+// console.log("a + b =", a + b);  //addition
+// console.log("a - b =", a - b);  //substraction
+// console.log("a * b =", a * b);  //multiplication
+// console.log("a / b =", a / b);  //divition
+// console.log("a % b =", a % b);  //modulus
+// console.log("a ** b =", a ** b);  //exponentiation
+
+
+// Unary Operator
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, "& b = ", b);
+
+// console.log("++a = ", ++a);  //increment
+// console.log(a);
+
+// console.log("a++ = ", a++);  //increment
+// console.log(a);
+
+// console.log("--a = ", --a);  //decrement
+// console.log(a);
+
+// console.log("a-- = ", a--);  //decrement
+// console.log(a);
+
+
+// Assignment Operators
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 4 = 9
+// a **= 4 // a = a ** 4 = 625
+
+
+// Comparison Oprators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, "& b = ", b);
+
+// console.log("a == b", a == b);
+// console.log("a != b", a != b);
+
+// let c = 5;
+// let d = "5";
+
+// console.log("c = ", c, "& d = ", d);
+
+// console.log("c === d", c === d);
+// console.log("c !== d", c !== d);
+
+// console.log("5 < 2", a < b);
+// console.log("5 > 2", a > b);
+// console.log("5 <= 5", c <= d);
+// console.log("5 >= 5", c >= d);
+
+
+// Logical Oprators
+// let a = 6;
+// let b = 5;
+
+// console.log("cond1 && cond2", a > b && a === 6)  // && : ye oprator dekhta hai k koi bhi aik condition false hui tw result false hoga baqi true.
+
+// console.log("cond1 || cond2", a > b || a === 4)  // && : ye oprator dekhta hai k koi bhi aik condition true hui tw result true hoga baqi false.
+
+// console.log("cond1 ! cond2", !(a === 6))  // && : ye oprator true value false or false value ko true kr deta hai.
+
+
