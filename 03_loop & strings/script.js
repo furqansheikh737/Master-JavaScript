fecad8ea84f69b32d09cea6d76b7f7e83ad07508
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-22 00:53:46 +0500

    Loops done

---
+++ 03_loop & strings/script.js
@@ -0,0 +1,99 @@
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+
+
+//                                                             For Loops
+
+// for(initilization; stoping condition; updation){};
+
+// for(let i = 1; i <= 10000; i++){
+//     console.log("Pakistan")
+// }
+
+
+// calculate sum of 1 to 100
+
+// let sum = 0;
+
+// for(let i=1; i<=10; i++){
+//     sum = sum + i;
+//     console.log(sum)
+// }
+// console.log("sum = ",sum)
+
+
+//                                                               While Loop
+
+// let i = 1;
+// while(i<=10){
+//     console.log("pakistan");
+//     i++;
+// }
+
+
+//                                                               Do-While Loop
+
+// let i = 20
+// do{
+//     console.log("apna college");
+//     i++;
+// } while (i <= 10);
+
+
+//                                                                 For-Of Loop (ye array or string pr lgta hai)
+
+// let str = "karachi"
+// let size = 0;
+// for(let i of str){
+//     console.log("i =", i)
+//     size++;
+// }
+// console.log("string size =", size)
+
+//                                                                 For-In Loop (ye object pr lgta hai)
+
+// let student = {
+//     name: "furqan",
+//     cgpa: 7.5,
+//     age: 22,
+//     isPassed: true,
+// };
+
+// for(let i in student){
+//    console.log("key =", i, "value =", student[i]);
+   
+// }
+
+
+//                                                                             Let,s Practice
+
+// Q1. Print all even number from 0 to 100
+
+// for(let i=0; i<=100; i++){
+//    if(i%2 === 0){            // even number
+//     console.log("i =", i)
+//    }
+// }
+
+// for(let i=0; i<=100; i++){
+//    if(i%2 === 1){            // odd number
+//     console.log("i =", i)
+//    }
+// }
+
+// Q2.
+
+// let gameNum = 33;
+// let userInput = prompt("Guess the game number");
+
+// while(gameNum != userInput){
+//     prompt("You entered wrong number. Guess again");
+// };
+
+// alert("Congratulaions, you entered the right number");
+
+
+//                                                               Strings
