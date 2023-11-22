388c296ac0b0d420a371da6339c7d8c97eb4cab7
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-22 21:12:02 +0500

    Strings and strings method done

--- 03_loop & strings/script.js
+++ 03_loop & strings/script.js
@@ -97,3 +97,81 @@
 
 
 //                                                               Strings
+
+// let userName = "My name is Furqan";
+// let schoolName = "Royal acadamy";
+// console.log(userName);
+// console.log(schoolName)
+
+//                                                             String Length
+
+// let str = "ApnaCollege"
+// console.log(str.length);
+
+//                                                             string Indices
+
+// let str = "ApnaCollege"
+// console.log(str[3]);  //a
+// console.log(str[5]);  //o
+
+//                                                            Template Literals
+
+// let specialStr = `This is a Template Literals`;
+// console.log(specialStr);
+
+
+// let obj = {
+//     item: "pen",
+//     price: 20,
+// }
+
+// let product = `The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(product);
+// console.log(typeof product);
+
+//                                                               Escape character
+
+// console.log("Apna\ncollege"); // \n = ye code ko next line ma lejata hai.
+// console.log("Apna\tCollege"); // \t = ye code ke beech ma space create kr deta hai.
+
+// let str = "Apna\ncollege"; //12
+// console.log(str.length)
+
+//                                                              Strings Methods (There are built-in functions to manipulate a string )
+
+// str.toUpperCase()
+// str.toLowerCase()
+// str.trim() // remore whitespaces
+// str.slice(start, end) // return part of string
+// str1.concat(str2) // join str2 with str1
+// str.replace(searchVal, newVal)
+// str1.charAt(idx)
+
+// let str1 = "abcdef";
+// console.log(str1.toUpperCase());
+
+// let str2 = "WXYZ";
+// console.log(str2.toLowerCase());
+
+// let str3 = "     pakistan zindabad       "
+// console.log(str3.trim());
+
+// let str4 = "karachi";
+// console.log(str4.slice(3, 6));
+
+// let str5 = "Apna";
+// let str6 = "College";
+// console.log(str5.concat(str6));
+
+// let str7 = "hello";
+// console.log(str7.replace("lo", "p"));
+
+// let str8 = "hello";
+// console.log(str8.charAt(4));
+
+//                                                                   Let's Practice
+
+// Q1. Prompt the user to enter their full name. Generate the username for them based on the input. Start username with @, followed by their full name and ending with the full name length?
+
+let userInput = prompt("Enter full name with no space")
+console.log(`@${userInput}${userInput.length}`)
