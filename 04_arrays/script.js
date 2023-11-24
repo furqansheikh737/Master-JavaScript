c05e06357507c2dbb84407693c9503604c9de549
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-25 00:07:44 +0500

    Arrays method done

--- 04_arrays/script.js
+++ 04_arrays/script.js
@@ -48,3 +48,72 @@
 //    items[i] -= offer;
 // }
 // console.log(items);
+
+//                                                                         Arrays Methods
+
+// push()       // add to end
+// pop()        // delete from end and return
+// toString()   // convert array to string
+// concat()     // join multiple array and return
+// unshift()    // add to start
+// shift()      // delete from start and return
+// slice()      // return the piece of the array
+// slice(startIdx, endIdx)
+// splice()     // change original array (add, remove, replace)
+// splice(startIdx, deleteCount, newElement)
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.push("peach", "litchi"));
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.pop());
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.toString());
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// let vagitable = ["tomato", "potato", "onion"];
+// let foodItems = furites.concat(vagitable);
+// console.log(foodItems);
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.unshift("litchi"));
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.shift());
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+// console.log(furites.slice(2, 4));
+
+// let furites = ["apple", "banana", "mango", "graphs", "orange"];
+
+// Add Element
+// furites.splice(2, 0, "potato");
+
+// Delete Element
+// furites.splice(2, 1);
+
+// Replace Element
+// furites.splice(2, 1, "litche");
+
+// furites.splice(2);   // splice is senerio ma index 2 k bad se sare value ko delete kr deta hai.
+
+
+// Q1. Create an array to store companies -> "Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+
+// a) Remove the first company from the array
+
+// console.log(companies.shift());
+
+// b) Remove Uber & add Ola in its place
+
+// console.log(companies.splice(2, 1, "Ola"));
+
+// c) Add Amazon at the end
+
+// console.log(companies.push("Amazon"));
+
+
+
