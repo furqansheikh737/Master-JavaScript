68c061398ff0bf0092fb8a69fb118dbe81c2992a
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-26 01:26:19 +0500

    functions done

---
+++ 05_function & methods/script.js
@@ -0,0 +1,67 @@
+//                                                          Functions
+// Block od code that performs a specific task, can be invoked whenever needed.
+
+// function myFunction(){
+//    console.log("Welcom to coding industry");
+// }
+
+// myFunction();
+// myFunction();
+
+// function myFunction(msg){  // parameter
+//    console.log(msg);
+// }
+
+// myFunction("My name is Furqan"); // argument
+
+// sum function
+// function sum(a, b){
+//    return a + b;
+// }
+// sum(4, 5);
+
+// const sumArrow = (a, b) => {
+//     return a + b;
+// }
+// sumArrow(19, 1);
+
+// multiplication function
+// function mul(a, b){
+//     return a * b;
+// }
+// mul(2,6);
+
+// const mulArrow = () => {
+//     return a * b;
+// }
+// mulArrow(33, 7);
+
+//                                                                Practice Question
+
+// Q1. Create a function using the "function keyword that takes a String as an argument & return the number of vowels in the string.
+
+// function vowel(str) {
+// let count = 0
+//     for (let val of str) {
+//         if (val === "a" || val === "e" || val === "i" || val === "o" || val === "u") {
+//             count++
+//         };
+//     };
+//     console.log(count)
+
+// };
+
+// vowel(`${prompt("enter a words")}`);
+
+// Q2. Create same task from arrow function.
+
+// const vowelCount = (str) => {
+//     let count = 0
+//     for (let val of str) {
+//         if (val === "a" || val === "e" || val === "i" || val === "o" || val === "u") {
+//             count++
+//         };
+//     };
+//     console.log(count)
+
+// }
