e3ae379e9dcfe3dd59874bfc2eeb674d676edc02
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-11-27 17:45:11 +0500

    Filter and reduce functions done

--- 05_function & methods/script.js
+++ 05_function & methods/script.js
@@ -105,3 +105,80 @@
 //     return num ** 2;
 // })
 // console.log(newNum);
+
+//                                                             Filter Function
+
+// Filter : Create a new array of elements that give true for a condition/filter.
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let newNum = numbers.filter((val) => {
+//     return val % 2 === 0
+// });
+
+// console.log(newNum);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let newNum = numbers.filter((val) => {
+//     return val > 5
+// });
+
+// console.log(newNum);
+
+//                                                             Reduce Function
+
+// Reduce : Performs some oprations & reduce the array to a single value. It return that single value.
+
+let numbers = [1, 2, 3, 4];
+
+let sum = numbers.reduce((prev, curr) => {
+    return prev + curr;
+});
+
+console.log(sum);
+
+
+let output = numbers.reduce((prev, curr) => {
+    return prev > curr ? prev : curr;
+});
+
+console.log(output);
+
+
+//                                                             Practice Question
+
+// Q1. We are given array of marks of students. filter out of the marks of student that scored 90+.
+
+let marks = [80, 90, 99, 45, 92, 67, 78, 95];
+
+let topperMarks = marks.filter((val) => {
+    return val > 90;
+})
+console.log(topperMarks);
+
+// Q2. 
+
+let n = prompt("Enter a number");
+
+let arr = [];
+
+for(let i=1; i<=n; i++){
+   arr[i-1] = i;
+}
+console.log(arr);
+
+let sums = arr.reduce((prev, curr) => {
+    return prev + curr;
+})
+
+console.log(sums);
+
+
+let factorial = arr.reduce((prev, curr) => {
+    return prev * curr;
+});
+
+console.log(factorial);
+
+
