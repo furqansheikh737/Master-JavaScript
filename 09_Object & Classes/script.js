5f8e498ccf1c0f7af9af4f734f16e9afb4a5b988
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-02-21 01:22:01 +0500

    practice jacaScript object question

--- 09_Object & Classes/script.js
+++ 09_Object & Classes/script.js
@@ -1,211 +1,258 @@
-//                                                           Objects
+// //                                                           Objects
 
-// A javaScript oject is an entity having state and behavior (properties and method).
+// // A javaScript oject is an entity having state and behavior (properties and method).
 
-// JavaScript object have a special called prototype.
+// // JavaScript object have a special called prototype.
 
-// We can set prototype using __proto__
+// // We can set prototype using __proto__
 
-// this keyword excess krta hai apne nearest object ki property ko
+// // this keyword excess krta hai apne nearest object ki property ko
 
-const student = {
-    name: "Furqan Sheikh",
-    marks: 78,
-    printMarks: function () {
-        console.log("marks = ", this.marks);
-    },
-};
+// const student = {
+//     name: "Furqan Sheikh",
+//     marks: 78,
+//     printMarks: function () {
+//         console.log("marks = ", this.marks);
+//     },
+// };
 
-// jab bhi koi object ya array banta hai tw prototype k general method ya properties hen wo by default inherit ho kr ajate hen.
+// // jab bhi koi object ya array banta hai tw prototype k general method ya properties hen wo by default inherit ho kr ajate hen.
 
-const employee = {
-    caltax() {
-        console.log("tax rate in 10%")
-    }
-}
+// const employee = {
+//     caltax() {
+//         console.log("tax rate in 10%")
+//     }
+// }
 
-const osama = {
-    salary: 5000,
-};
+// const osama = {
+//     salary: 5000,
+// };
 
-// if object & prototype have same method, object,s method will be used.
-const osama2 = {
-    salary: 3000,
-    caltax() {
-        console.log("tax rate in 30%")
-    }
-};
+// // if object & prototype have same method, object,s method will be used.
+// const osama2 = {
+//     salary: 3000,
+//     caltax() {
+//         console.log("tax rate in 30%")
+//     }
+// };
 
-const osama3 = {
-    salary: 4000,
-};
+// const osama3 = {
+//     salary: 4000,
+// };
 
-const osama4 = {
-    salary: 6000,
-};
-osama.__proto__ = employee;
-osama2.__proto__ = employee;
-osama3.__proto__ = employee;
-osama4.__proto__ = employee;
+// const osama4 = {
+//     salary: 6000,
+// };
+// osama.__proto__ = employee;
+// osama2.__proto__ = employee;
+// osama3.__proto__ = employee;
+// osama4.__proto__ = employee;
 
-//                                                        Classes
+// //                                                        Classes
 
-// Classes is a program-code template for creating objects.
+// // Classes is a program-code template for creating objects.
 
-// Those object will have some state (variables) & some behavior (functions) inside it.
+// // Those object will have some state (variables) & some behavior (functions) inside it.
 
-class ToyotaCar {
-    // Object ko initialize karate waqt hamein koi kam karana hota hai tw hum construct k through karate hen 
-    constructor(brand, mileage) {
-        // console.log("creating new object")
-        this.brandName = brand
-        this.mileage = mileage
-    }
+// class ToyotaCar {
+//     // Object ko initialize karate waqt hamein koi kam karana hota hai tw hum construct k through karate hen 
+//     constructor(brand, mileage) {
+//         // console.log("creating new object")
+//         this.brandName = brand
+//         this.mileage = mileage
+//     }
 
-    start() {
-        console.log("start")
-    }
-    stop() {
-        console.log("stop")
-    }
+//     start() {
+//         console.log("start")
+//     }
+//     stop() {
+//         console.log("stop")
+//     }
 
-    setBrand(brand) {
-        this.brand = brand;
-    }
-}
+//     setBrand(brand) {
+//         this.brand = brand;
+//     }
+// }
 
-let corola = new ToyotaCar("Toyota", 20);// jab bhi new object bane ga tab constructor call hoga
-// console.log(corola)
-// fortuner.setBrand("KIA")
-let civic = new ToyotaCar("honda", 35);//jab bhi new object bane ga tab constructor call hoga
-// console.log(civic)
-// honda.setBrand("lexus")
-
-
-//                                                              Inheritance
-
-// Inheritance is passing down properties & methods from parent class to child class.
-
-// Example 1
-class Parent {
-    hello() {
-        console.log("Hello bahi")
-    }
-}
-
-class Child extends Parent { }
-
-let obj = new Child();
+// let corola = new ToyotaCar("Toyota", 20);// jab bhi new object bane ga tab constructor call hoga
+// // console.log(corola)
+// // fortuner.setBrand("KIA")
+// let civic = new ToyotaCar("honda", 35);//jab bhi new object bane ga tab constructor call hoga
+// // console.log(civic)
+// // honda.setBrand("lexus")
 
 
-// Example 2
-class Person {
-    constructor(){
-        this.species = "homo sapiens"
-    }
+// //                                                              Inheritance
 
-    eat(){
-        console.log("eat")
-    }
+// // Inheritance is passing down properties & methods from parent class to child class.
 
-    sleep(){
-        console.log("sleep")
-    }
+// // Example 1
+// class Parent {
+//     hello() {
+//         console.log("Hello bahi")
+//     }
+// }
 
-        // if child & parent have same method, child's method will be used. [Method Overriding]
-    work(){
-        console.log("do nothing")
-    }
-}
+// class Child extends Parent { }
 
-class Engineer extends Person{
-        // if child & parent have same method, child's method will be used. [Method Overriding]
-    work(){
-        console.log("solve problem, build somethings")
-    }
-}
-
-class Doctor extends Person{
-        // if child & parent have same method, child's method will be used. [Method Overriding]
-    work(){
-        console.log("treat patients")
-    }
-}
-
-let furqan = new Engineer();
-
-// Example 3
-class Persons {
-    constructor(name){
-        console.log("call parent constructor")
-        this.species = "homo sapiens"
-        this.name = name;
-    }
-
-    eat(){
-        console.log("eat")
-    }
-}
-
-class Engineers extends Persons{
-    constructor(name){
-        console.log("call child constructor")
-        super(name) // to invoke parent class constructor
-    //    this.branch = branch;
-       console.log("exit child constructor")
-    }
-
-    work(){
-        super.eat();
-        console.log("solve problem, build somethings")
-    }
-}
-
-// let burhan = new Engineers("chemical engineer");
-let burhan = new Engineers("shahzaib");
+// let obj = new Child();
 
 
-//                                                    Practice questions
-// Q1) You are creating a website for your collage. Create a class User with 2 properties, name & email. It also has a method called viewDara() that allow user to view website data.
+// // Example 2
+// class Person {
+//     constructor(){
+//         this.species = "homo sapiens"
+//     }
 
-let data = "secret information"
-class User {
-    constructor(name, email){
-        this.name = name;
-        this.email = email
-    }
+//     eat(){
+//         console.log("eat")
+//     }
 
-    viewData(){
-        console.log(`data = ${data}`);
-    }
-}
+//     sleep(){
+//         console.log("sleep")
+//     }
 
-let student1 = new User("furqan", "email@.125com")
-let student2 = new User("hamza", "email@123.com")
-let teacher1 = new User("burhan", "email@124.com")
+//         // if child & parent have same method, child's method will be used. [Method Overriding]
+//     work(){
+//         console.log("do nothing")
+//     }
+// }
 
-// Q2) Create a new class called Admin which inherits from User. Add a new method called editData to Admin that allows it to edit website data.
+// class Engineer extends Person{
+//         // if child & parent have same method, child's method will be used. [Method Overriding]
+//     work(){
+//         console.log("solve problem, build somethings")
+//     }
+// }
 
-let DATA = "secret information"
-class User1 {
-    constructor(name, email){
-        this.name = name;
-        this.email = email
-    }
+// class Doctor extends Person{
+//         // if child & parent have same method, child's method will be used. [Method Overriding]
+//     work(){
+//         console.log("treat patients")
+//     }
+// }
 
-    viewData(){
-        console.log(`data = ${DATA}`);
-    }
-}
+// let furqan = new Engineer();
 
-class Admin extends User1 {
-    constructor(name, email){
-     super(name, email)
-    }
+// // Example 3
+// class Persons {
+//     constructor(name){
+//         console.log("call parent constructor")
+//         this.species = "homo sapiens"
+//         this.name = name;
+//     }
 
-    editData(){
-      DATA = "update secret information"  
-    }
-}
+//     eat(){
+//         console.log("eat")
+//     }
+// }
 
-let admin = new Admin("Admin", "admin@admin.com");
+// class Engineers extends Persons{
+//     constructor(name){
+//         console.log("call child constructor")
+//         super(name) // to invoke parent class constructor
+//     //    this.branch = branch;
+//        console.log("exit child constructor")
+//     }
+
+//     work(){
+//         super.eat();
+//         console.log("solve problem, build somethings")
+//     }
+// }
+
+// // let burhan = new Engineers("chemical engineer");
+// let burhan = new Engineers("shahzaib");
+
+
+// //                                                    Practice questions
+// // Q1) You are creating a website for your collage. Create a class User with 2 properties, name & email. It also has a method called viewDara() that allow user to view website data.
+
+// let data = "secret information"
+// class User {
+//     constructor(name, email){
+//         this.name = name;
+//         this.email = email
+//     }
+
+//     viewData(){
+//         console.log(`data = ${data}`);
+//     }
+// }
+
+// let student1 = new User("furqan", "email@.125com")
+// let student2 = new User("hamza", "email@123.com")
+// let teacher1 = new User("burhan", "email@124.com")
+
+// // Q2) Create a new class called Admin which inherits from User. Add a new method called editData to Admin that allows it to edit website data.
+
+// let DATA = "secret information"
+// class User1 {
+//     constructor(name, email){
+//         this.name = name;
+//         this.email = email
+//     }
+
+//     viewData(){
+//         console.log(`data = ${DATA}`);
+//     }
+// }
+
+// class Admin extends User1 {
+//     constructor(name, email){
+//      super(name, email)
+//     }
+
+//     editData(){
+//       DATA = "update secret information"  
+//     }
+// }
+
+// let admin = new Admin("Admin", "admin@admin.com");
+
+// Q3) Address Book: Create an address book where each entry contains a person's name, address, and phone number. Implement functions to add new entries, delete entries, and search for a specific person's information.
+
+// Define the AddressBook constructor function
+function AddressBook() {
+    this.entries = [];
+  }
+  
+  // Define the Entry constructor function
+  function Entry(name, address, phoneNumber) {
+    this.name = name;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+  }
+  
+  // Add a method to AddressBook to add a new entry
+  AddressBook.prototype.addEntry = function(name, address, phoneNumber) {
+    var newEntry = new Entry(name, address, phoneNumber);
+    this.entries.push(newEntry);
+  };
+  
+  // Add a method to AddressBook to delete an entry by name
+  AddressBook.prototype.deleteEntry = function(name) {
+    this.entries = this.entries.filter(function(entry) {
+      return entry.name !== name;
+    });
+  };
+  
+  // Add a method to AddressBook to search for an entry by name
+  AddressBook.prototype.searchEntry = function(name) {
+    var foundEntry = this.entries.find(function(entry) {
+      return entry.name === name;
+    });
+    return foundEntry || "Entry not found";
+  };
+  
+  // Example usage
+  var myAddressBook = new AddressBook();
+  myAddressBook.addEntry("John Doe", "123 Main St", "555-1234");
+  myAddressBook.addEntry("Jane Smith", "456 Elm St", "555-5678");
+  console.log(myAddressBook.entries);
+  
+  myAddressBook.deleteEntry("John Doe");
+  console.log(myAddressBook.entries);
+  
+  console.log(myAddressBook.searchEntry("Jane Smith"));
+  console.log(myAddressBook.searchEntry("John Doe"));
