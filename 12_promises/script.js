c62df6af5b0b03f70c0f5d43e158d736235e6fec
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-03-12 00:53:32 +0500

    practice js async await questions

--- 12_promises/script.js
+++ 12_promises/script.js
@@ -31,7 +31,7 @@ const promiseThree = new Promise(function(resolve, reject){
 })
 
 promiseThree.then(function(user){
-  console.log(user)
+//   console.log(user)
 })
 
 const promiseFour = new Promise(function(resolve, reject){
@@ -42,21 +42,66 @@ const promiseFour = new Promise(function(resolve, reject){
         if (!error) {
             resolve({user: 'hamza', email: 'hamza@gmail.com'})
         }else{
-            reject(console.log("something went wrong"))
+            // reject(console.log("something went wrong"))
         }
     }, 1000)
 })
 
 promiseFour
 .then((user) => {
-  return console.log(user.name)
+//   return console.log(user.name)
 })
 .then((data) => {
-  console.log(data)
+//   console.log(data)
 })
 .catch((error) => {
-  console.log(error)
+//   console.log(error)
 })
 .finally(() => {
- console.log("The promise is either resolve or rejected")
+//  console.log("The promise is either resolve or rejected")
+})
+
+const promiseFive = new Promise((resolve, reject) => {
+   setTimeout(() => {
+    let error = true
+    if(!error) {
+        resolve({username: "javaScript", password: "12345"})
+    }else{
+        reject("Error: JS went wrong")
+    }
+   }, 1000);
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        // console.log(response)
+    } catch (error) {
+        // console.log(error)
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUser(){
+//    try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json();
+//     console.log(data)
+//    } catch (error) {
+//     console.log(error)
+//    }
+// }
+
+// getAllUser()
+
+fetch("https://jsonplaceholder.typicode.com/users/10")
+.then((response) => {
+       return response.json()
+})
+.then((data) => {
+console.log(data)
+})
+.catch((error) => {
+    console.log(error)
 })
