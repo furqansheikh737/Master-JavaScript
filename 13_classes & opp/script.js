913bc575a7c968f415371af9a29780de32f454c8
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-03-14 01:28:49 +0500

    javaScript array, object, and oop concept

--- 13_classes & opp/script.js
+++ 13_classes & opp/script.js
@@ -30,5 +30,80 @@ function User(username, loginCount, isLoggedIn){
 const output1 = new User('Furqan', 12, false);
 const output2 = new User('Hamza', 13, true);
 
-console.log(output1.constructor)
-console.log(output2)
+// console.log(output1.constructor)
+// console.log(output2)
+
+
+function multipleBy5(num){
+
+    return num*5
+}
+
+multipleBy5.power = 2
+
+// console.log(multipleBy5(5));
+// console.log(multipleBy5.power);
+// console.log(multipleBy5.prototype);
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    console.log(this.score++)
+}
+createUser.prototype.print = function(){
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai", 50)
+const tea = new createUser("tea", 250)
+
+// tea.print()
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
+
+//                                                               Prototype
+
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
